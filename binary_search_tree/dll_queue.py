54f527c24d996ed85f956dc3d13482c813ce8629
Vaeliwen <zaman@imbriaco.com>
2020-01-29 15:23:30 -0500

    Added required dependencies to binary_search_tree folder.

--- binary_search_tree/dll_queue.py
+++ binary_search_tree/dll_queue.py
@@ -1,19 +1,23 @@
 import sys
 from doubly_linked_list import DoublyLinkedList
 
-class Stack:
+
+class Queue:
+
     def __init__(self):
         self.size = 0
         self.storage = DoublyLinkedList()
 
-    def push(self, value):
-        self.storage.add_to_tail(value)
+    def enqueue(self, value):
+        self.storage.add_to_head(value)
 
-    def pop(self):
+    def dequeue(self):
         if self.storage.tail != None:
             return self.storage.remove_from_tail()
+            
         else:
-            return self.storage.tail
+            return None
 
     def len(self):
         return self.storage.__len__()
+
